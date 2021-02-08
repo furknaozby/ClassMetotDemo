7a7ca56e7df3aa8de3ad92be5cbca98a4d8315cd
Furkan Özbay <furkanozb37@hotmail.com>
2021-02-09 01:19:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.Soyad+"Musteri Eklendi");
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id+"-)"+musteri.Ad+" "+musteri.Soyad+" - "+musteri.TCKN);
+            }
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" nolu Musteri Silindi ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri[] musteriler = new Musteri[]
+            {
+                new Musteri{Id=1,Ad="Fuat Atıf",Soyad="Tufa",TCKN="100000000000"},
+                new Musteri{Id=2,Ad="Nuh ",Soyad="Tufan",TCKN="100000000000"},
+                new Musteri{Id=3,Ad="Rıza ",Soyad="Silahlıpoda",TCKN="100000000000"},
+            };
+
+            MusteriManager manager = new MusteriManager();
+
+            Console.WriteLine("----------Musteri Listleleme----------");
+            manager.MusteriListele(musteriler);
+
+            Console.WriteLine("----------Musteri Silme----------");
+            manager.MusteriSil(musteriler[0]);
+
+            Console.WriteLine("----------Musteri Ekleme----------");
+            manager.MusteriEkle(musteriler[2]);
+        }
+    }
+}
